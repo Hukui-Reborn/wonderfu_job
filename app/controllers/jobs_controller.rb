26fb8af739a4e4899862254041bd2229f326686e
hukui <tckuikui1234>
2017-01-10 13:20:38 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @jobs = Job.all
@@ -12,13 +13,32 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def create
-    @job = Job.new(jop_params)
+    @job = Job.new(job_params)
     if @job.save
       redirect_to jobs_path
     else
       render :new
     end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Group Deleted"
   end
 
   private
