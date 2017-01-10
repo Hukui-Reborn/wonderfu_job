5fc6c6dd1de9959d57c130bf2304ee59fbb56d59
hukui <tckuikui1234>
2017-01-10 13:34:10 +0800

    add job_controller all actions

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,4 @@
-class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
+class Admin::JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
@@ -13,14 +11,10 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
-
   def create
     @job = Job.new(job_params)
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
@@ -29,20 +23,19 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path, notice: "Update Success"
+      redirect_to admin_jobs_path, notice: "Update Success"
     else
       render :edit
     end
   end
 
   def destroy
-    @job = Job.find(params[:id])
+    @job = Job.find(parmas[:id])
     @job.destroy
-    redirect_to jobs_path, alert: "Job Deleted"
+    redirect_to admin_jobs_path, alert: "Job Deleted"
   end
 
   private
-
   def job_params
     params.require(:job).permit(:title, :description)
   end
